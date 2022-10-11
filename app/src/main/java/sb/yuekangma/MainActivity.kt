10b54a782e1bd24b02e156cfe7610aede3ad8cdb
mahaocheng <reg@thu.news>
2022-10-11 11:54:36 +0800

    show status bar and navigation bar; adjust new-style yuekangma; remove unit test examples
    upgrade build components

--- app/src/main/java/sb/yuekangma/MainActivity.kt
+++ app/src/main/java/sb/yuekangma/MainActivity.kt
@@ -1,57 +1,51 @@
-package com.example.myapplication
+package sb.yuekangma
 
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.text.format.DateFormat
-import android.view.View
+import android.view.WindowInsets
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.VideoView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
-
-
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-class FullscreenActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var videoView: VideoView
 
+    @RequiresApi(Build.VERSION_CODES.R)
     @SuppressLint("ClickableViewAccessibility", "SdCardPath")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_fullscreen)
-
+        // check write external storage permission
         val hasWriteExternalPermission =
-            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
         if (hasWriteExternalPermission != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "need READ_EXTERNAL_STORAGE permission", Toast.LENGTH_LONG).show()
         }
 
+        // set system UIs like bars
+        supportActionBar?.hide()
+        window.setDecorFitsSystemWindows(false)
+        window.decorView.windowInsetsController?.show(WindowInsets.Type.statusBars())
+        window.decorView.windowInsetsController?.show(WindowInsets.Type.systemBars())
+        setContentView(R.layout.main_activity)
+
+        // display date and time text
         val testTimeText:TextView = findViewById(R.id.testTimeText)
         val testTime = System.currentTimeMillis() - 51123456
         testTimeText.text = DateFormat.format("yyyy-MM-dd HH:mm", testTime)
 
+        // play background video
         videoView = findViewById(R.id.videoView)
         videoView.setVideoPath("/sdcard/code/ykm.mp4")
-
         videoView.setOnPreparedListener { videoView.start() }
         videoView.setOnCompletionListener{ videoView.start() }
-
         videoView.requestFocus()
-
-        videoView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
-
 }
